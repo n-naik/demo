434146a9e553f9734be68d4bbc75dbf2497eeab6
Nilesh Naik (POBS-TCS) <N_Naik@acs.org>
2025-08-13 18:48:30 +0530

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testMainMethod() {
+        // You can add tests here to validate the functionality of the App class
+        assertEquals(1, 1); // Placeholder assertion
+    }
+}
